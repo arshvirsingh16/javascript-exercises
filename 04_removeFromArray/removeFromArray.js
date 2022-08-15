744d738b1c6feeb4a9677075ffe98382b4e8bfad
Arshvir Singh <arshvirsignh16@gmail.com>
2022-08-14 20:14:11 -0600

    First test succes for removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function replace(array, num) {
+    let newArray = array;
+    let length = newArray.length;
 
+    for(i = 0; i < length; i++) {
+        if(num == newArray[i]) {
+            newArray.splice(i , 1);
+        }
+    }
+    
+    let changedArray = newArray;
+    return changedArray;
 };
 
 // Do not edit below this line
