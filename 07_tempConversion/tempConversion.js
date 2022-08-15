497637b801d147da23a4402265b658bdd5809bdd
Arshvir Singh <arshvirsignh16@gmail.com>
2022-08-15 11:37:23 -0600

    All tests work

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,29 @@
-const ftoc = function() {
+const ftoc = function(farenheight) {
+  
+  var celcius;
+
+  let x = farenheight - 32;
+  const y = 5/9;
+
+  celcius = x * y;
+
+  let rounded = Math.round(celcius*10) / 10;
+
+  return rounded;
 
 };
 
-const ctof = function() {
+const ctof = function(celcius) {
+
+  var faren;
+
+  let x = celcius * (9/5);
+
+  faren = x + 32;
+
+  let rounded = Math.round(faren * 10) / 10;
+
+  return rounded;
 
 };
 
