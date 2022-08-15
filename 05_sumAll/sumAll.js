3c3980ba136bb5942f31c687b0f1d3a76897855a
Arshvir Singh <arshvirsignh16@gmail.com>
2022-08-14 20:49:21 -0600

    Passes 5 of 7 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    var totalSum = 0;
+
+    if(num1 < 0 || num1 == "" || num1 == []){
+        return "ERROR";
+    }
+
+    
+    if(num2 < 0 || num2 == "" || num2 == []){
+        return "ERROR";
+    }
+
+    if(num1 > num2){
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    for(i = num1; i < num2 + 1; i++) {
+        totalSum += i;
+    }
+
+    return totalSum;
 
 };
 
